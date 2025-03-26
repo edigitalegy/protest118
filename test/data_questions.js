065dbe2f852c0020cdf7cfc465ea45a1bb8c9367
edigitalegy <130320276+edigitalegy@users.noreply.github.com>
2025-03-26 03:09:02 +0200

    Update data_questions.js

--- test/data_questions.js
+++ test/data_questions.js
@@ -1,6 +1,6 @@
 const quizData = [
   {
-    header: "./img/ship.PNG",
+    header: "./img/ship.png",
     question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
     optionstype: "img",
     optionssrc: ["./img/sh1.png","./img/sh2.png","./img/sh3.png","./img/sh4.png","./img/sh5.png","./img/sh6.png","./img/sh7.png","./img/sh8.png"],
@@ -11,7 +11,7 @@ const quizData = [
     modalType: ""
   },
   {
-    header: "./img/telephone.PNG",
+    header: "./img/telephone.png",
     question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
     optionstype: "img",
     optionssrc: ["./img/tel1.png","./img/tel2.png","./img/tel3.png","./img/tel4.png","./img/tel5.png","./img/tel6.png","./img/tel7.png","./img/tel8.png"],
@@ -22,7 +22,7 @@ const quizData = [
     modalType: ""
   },
   {
-    header: "./img/bird.PNG",
+    header: "./img/bird.png",
     question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
     optionstype: "img",
     optionssrc: ["./img/bi1.png","./img/bi2.png","./img/bi3.png","./img/bi4.png","./img/bi5.png","./img/bi6.png","./img/bi7.png","./img/bi8.png"],
@@ -32,7 +32,7 @@ const quizData = [
     modal: "",
     modalType: ""
   },{
-  header: "./img/man.PNG",
+  header: "./img/man.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/ma1.png","./img/ma2.png","./img/ma3.png","./img/ma4.png","./img/ma5.png","./img/ma6.png","./img/ma7.png","./img/ma8.png"],
@@ -43,7 +43,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/lion.PNG",
+  header: "./img/lion.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/li1.png","./img/li2.png","./img/li3.png","./img/li4.png","./img/li5.png","./img/li6.png","./img/li7.png","./img/li8.png"],
@@ -54,7 +54,7 @@ const quizData = [
   modalType: ""
 },
 {
-  header: "./img/apple.PNG",
+  header: "./img/apple.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/ap1.png","./img/ap2.png","./img/ap3.png","./img/ap4.png","./img/ap5.png","./img/ap6.png","./img/ap7.png","./img/ap8.png"],
@@ -65,7 +65,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/pen.PNG",
+  header: "./img/pen.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/pe1.png","./img/pe2.png","./img/pe3.png","./img/pe4.png","./img/pe5.png","./img/pe6.png","./img/pe7.png","./img/pe8.png"],
@@ -76,7 +76,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/boot.PNG",
+  header: "./img/boot.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/bo1.png","./img/bo2.png","./img/bo3.png","./img/bo4.png","./img/bo5.png","./img/bo6.png","./img/bo7.png","./img/bo8.png"],
@@ -87,7 +87,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/fiesh.PNG",
+  header: "./img/fiesh.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/fi1.png","./img/fi2.png","./img/fi3.png","./img/fi4.png","./img/fi5.png","./img/fi6.png","./img/fi7.png","./img/fi8.png"],
@@ -98,7 +98,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/watch.PNG",
+  header: "./img/watch.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/wat1.png","./img/wat2.png","./img/wat3.png","./img/wat4.png","./img/wat5.png","./img/wat6.png","./img/wat7.png","./img/wat8.png"],
@@ -109,7 +109,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/glass.PNG",
+  header: "./img/glass.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/gl1.png","./img/gl2.png","./img/gl3.png","./img/gl4.png","./img/gl5.png","./img/gl6.png","./img/gl7.png","./img/gl8.png"],
@@ -120,7 +120,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/tree.PNG",
+  header: "./img/tree.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/tr1.png","./img/tr2.png","./img/tr3.png","./img/tr4.png","./img/tr5.png","./img/tr6.png","./img/tr7.png","./img/tr8.png"],
@@ -131,7 +131,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/car.PNG",
+  header: "./img/car.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/ca1.png","./img/ca2.png","./img/ca3.png","./img/ca4.png","./img/ca5.png","./img/ca6.png","./img/ca7.png","./img/ca8.png"],
@@ -142,7 +142,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/map.PNG",
+  header: "./img/map.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/map1.png","./img/map2.png","./img/map3.png","./img/map4.png","./img/map5.png","./img/map6.png","./img/map7.png","./img/map8.png"],
@@ -153,7 +153,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/face.PNG",
+  header: "./img/face.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/face1.png","./img/face2.png","./img/face3.png","./img/face4.png","./img/face5.png","./img/face6.png","./img/face7.png","./img/face8.png"],
@@ -164,7 +164,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/prush.PNG",
+  header: "./img/prush.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/pr1.png","./img/pr2.png","./img/pr3.png","./img/pr4.png","./img/pr5.png","./img/pr6.png","./img/pr7.png","./img/pr8.png"],
@@ -175,7 +175,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/cam.PNG",
+  header: "./img/cam.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/cam1.png","./img/cam2.png","./img/cam3.png","./img/cam4.png","./img/cam5.png","./img/cam6.png","./img/cam7.png","./img/cam8.png"],
@@ -186,7 +186,7 @@ const quizData = [
   modalType: ""
 }
 ,{
-  header: "./img/flower.PNG",
+  header: "./img/flower.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/fl1.png","./img/fl2.png","./img/fl3.png","./img/fl4.png","./img/fl5.png","./img/fl6.png","./img/fl7.png","./img/fl8.png"],
@@ -197,7 +197,7 @@ const quizData = [
   modalType: ""
 },
 {
-  header: "./img/spider.PNG",
+  header: "./img/spider.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/spi1.png","./img/spi2.png","./img/spi3.png","./img/spi4.png","./img/spi5.png","./img/spi6.png","./img/spi7.png","./img/spi8.png"],
@@ -208,7 +208,7 @@ const quizData = [
   modalType: ""
 },
 {
-  header: "./img/tv.PNG",
+  header: "./img/tv.png",
   question: "  اختر الصورة التي تتطابق مع الصورة الأصلية ",
   optionstype: "img",
   optionssrc: ["./img/tv1.png","./img/tv2.png","./img/tv3.png","./img/tv4.png","./img/tv5.png","./img/tv6.png","./img/tv7.png","./img/tv8.png"],
